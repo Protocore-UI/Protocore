ff3a8508c127063ff95303f580b356f76d1cbde6
hegdeashwin <ashwin.hegde3@gmail.com>
2015-01-13 01:54:09 +0530

    Adds basic routes

--- src/apps/router/routes.js
+++ src/apps/router/routes.js
@@ -1,20 +1,20 @@
-define(["routie"], function() {
+define(["crossroads"], function(crossroads) {
 
 	'use strict';
 	
-	routie({
-		'': function() {
-			require(['views/homeView'], function(HomeView) {
-				var homeView = new HomeView();
-				homeView.initialize();
-			});
-		},
-		'about': function() {
-			require(['views/aboutView'], function(AboutView) {
-				var aboutView = new AboutView();
-				aboutView.initialize();
-			});
-		}
+	crossroads.addRoute('/', function(){
+		console.log("Home View");
+		require(['views/homeView'], function(HomeView) {
+			var homeView = new HomeView();
+			homeView.initialize();
+		});
 	});
-	
+
+	crossroads.addRoute('/about', function(){
+		console.log("About Us View");
+		require(['views/aboutView'], function(AboutView) {
+			var aboutView = new AboutView();
+			aboutView.initialize();
+		});
+	});
 });
