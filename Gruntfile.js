f4daa3fa61b5341c3e2f4136b25c226ea9dd7e85
Ashwin Hegde <ashwinh@cybage.com>
2015-04-28 19:24:26 +0530

    migrates r.js to bower_components

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,8 +21,8 @@ module.exports = function(grunt) {
         shell: {
             uglify: {
                 command: [
-                    'node config/r.js -o config/build/optimize-build.js',
-                    'node config/r.js -o config/build/copy-build.js',
+                    'node src/bower_components/rjs/dist/r.js -o config/build/optimize-build.js',
+                    'node src/bower_components/rjs/dist/r.js -o config/build/copy-build.js',
                     'rm src/main-optimize.js',
                     'rm prod/main.js',
                     'mv prod/main-optimize.js prod/main.js'
